474797080990bb12bfd3786702f10584f365a811
lucid-techwrld <davisredtiger@gmail.com>
2025-08-15 01:48:49 -0700

    getting ready for prod

--- server/database/database.ts
+++ server/database/database.ts
@@ -2,6 +2,12 @@ import knex, { Knex } from "knex";
 const config = require("./knexfile");
 
 const environment = process.env.NODE_ENV || "development";
+console.log(`Using database configuration for environment: ${environment}`);
+if (!config[environment]) {
+  throw new Error(
+    `Database configuration for environment "${environment}" not found.`
+  );
+}
 const dbConfig: Knex.Config = config[environment];
 
 if (!dbConfig) {
--- server/database/knexfile.ts
+++ server/database/knexfile.ts
@@ -40,10 +40,12 @@ const config = {
     client: "postgresql",
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
     },
     pool: { min: 2, max: 10 },
-    migrations: { tableName: "knex_migrations" },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: path.resolve(__dirname, "migrations"),
+    },
   },
 };
 
