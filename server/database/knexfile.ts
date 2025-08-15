f75b55e43ea7ffefd2201b1589999ca96d311f10
lucid-techwrld <davisredtiger@gmail.com>
2025-08-15 10:40:56 -0700

    adj

--- server/database/knexfile.ts
+++ server/database/knexfile.ts
@@ -4,6 +4,16 @@ const dotenv = require("dotenv");
 // Load environment variables from root .env file
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
+const dbUrl = process.env.RENDER
+  ? process.env.DATABASE_URL
+  : process.env.DATABASE_URL_PROD_EXTERNAL;
+if (!dbUrl) {
+  throw new Error(
+    "DATABASE_URL or DATABASE_URL_PROD_EXTERNAL is not defined in the environment variables."
+  );
+}
+const isInternal = dbUrl.includes("internal");
+
 /** @type {import('knex').Knex.Config} */
 const config = {
   development: {
@@ -39,7 +49,8 @@ const config = {
   production: {
     client: "postgresql",
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      connectionString: dbUrl,
+      ...(isInternal ? {} : { ssl: { rejectUnauthorized: false } }),
     },
     pool: { min: 2, max: 10 },
     migrations: {
