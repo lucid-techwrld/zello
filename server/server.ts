2aa3ac36b3e4af66ad35c733dcb2f29abb77c767
lucid-techwrld <davisredtiger@gmail.com>
2025-08-18 02:23:28 -0700

    frontedn path2

--- server/server.ts
+++ server/server.ts
@@ -32,8 +32,8 @@ const corsOptions: CorsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-const FRONTEND_PATH = path.join(__dirname, "../client/dist");
-console.log("Frontend path:", path.join(FRONTEND_PATH, "index.html"));
+const FRONTEND_PATH = path.join(process.cwd(), "client/dist");
+console.log("Serving frontend from:", FRONTEND_PATH);
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(FRONTEND_PATH));
